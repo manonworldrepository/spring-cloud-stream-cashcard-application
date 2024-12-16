ec83036281f55b86f70f72efe2a34fe9a0e93f7a
manonworldrepository <info@manonworld.de>
2024-12-16 06:41:12 +0100

    Organized step defs

--- cashcard-transaction-e2e/src/test/java/com/example/demo/stepdef/Background.java
+++ cashcard-transaction-e2e/src/test/java/com/example/demo/stepdef/Background.java
@@ -1,9 +1,7 @@
 package com.example.demo.stepdef;
 
-import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.datatable.DataTable;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -21,25 +19,6 @@ public class Background {
 
     @Then("My sink application should be ready")
     public void mySinkApplicationShouldBeReady() {
-        assertTrue(true);
-    }
-
-    @Given("A user has the following cashcard")
-    public void aUserHasTheFollowingCashcard(DataTable dataTable) {
-        assertTrue(true);
-    }
-
-    @When("A user submits data")
-    public void a_user_submits_data() {
-        assertTrue(true);
-    }
-
-    @Then("No error should be retured")
-    public void noErrorShouldBeRetured() {
-    }
-
-    @Then("Data should be saved in a CSV file")
-    public void dataShouldBeSavedInACsvFile() {
         assertTrue(true);
     }
 
--- cashcard-transaction-e2e/src/test/java/com/example/demo/stepdef/StepDefinitions.java
+++ cashcard-transaction-e2e/src/test/java/com/example/demo/stepdef/StepDefinitions.java
@@ -1,9 +1,34 @@
 package com.example.demo.stepdef;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EmbeddedKafka(partitions = 1, topics = {"approval-requests", "enriched-transactions"})
 public class StepDefinitions {
 
+    @Given("A user has the following cashcard")
+    public void aUserHasTheFollowingCashcard(DataTable dataTable) {
+        assertTrue(true);
+    }
+
+    @When("A user submits data")
+    public void a_user_submits_data() {
+        assertTrue(true);
+    }
+
+    @Then("No error should be retured")
+    public void noErrorShouldBeRetured() {
+        assertTrue(true);
+    }
+
+    @Then("Data should be saved in a CSV file")
+    public void dataShouldBeSavedInACsvFile() {
+        assertTrue(true);
+    }
 
 }
